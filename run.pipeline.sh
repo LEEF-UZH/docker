9c56f7099c7a77d565c45bd47a622d3c01cce080
Rainer M. Krug <Rainer@krugs.de>
2020-11-16 16:11:16 +0100

    Update run.pipeline.sh

--- run.pipeline.sh
+++ run.pipeline.sh
@@ -4,7 +4,7 @@ TS=$(date '+%Y-%m-%d--%H-%M-%S')
 ##
 LF=./../0.raw.data/log.$TS.fast.txt
 touch $LF 
-make run.pipeline.fast &> | tee $LF
+make run.pipeline.fast &> $LF
 echo  >> $LF
 echo "##################################" >> $LF
 echo "## BEGIN: " $TS "##" >> $LF
@@ -14,7 +14,7 @@ echo "##################################" >> $LF
 ##
 # LF=./../0.raw.data/log.$TS.bemovi.mag.25.txt
 # touch $LF 
-# make run.pipeline.bemovi ID=mag.25 &> | tee $LF
+# make run.pipeline.bemovi ID=mag.25 &> $LF
 # echo  >> $LF
 # echo "##################################" >> $LF
 # echo "## BEGIN: " $TS "##" >> $LF
@@ -24,7 +24,7 @@ echo "##################################" >> $LF
 ##
 # LF=./../0.raw.data/log.$TS.bemovi.mag.16.txt
 # touch $LF 
-# make run.pipeline.bemovi ID=mag.16 &> | tee $LF
+# make run.pipeline.bemovi ID=mag.16 &> $LF
 # echo  >> $LF
 # echo "##################################" >> $LF
 # echo "## BEGIN: " $TS "##" >> $LF
