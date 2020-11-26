ee7a8668ecac060c9e69bd8154edc50dc161c9ad
Rainer M. Krug <Rainer@krugs.de>
2020-11-26 12:26:36 +0100

    Update run.pipeline.sh

--- run.pipeline.sh
+++ run.pipeline.sh
@@ -22,12 +22,12 @@ echo "## END:   " $(date '+%Y-%m-%d--%H-%M-%S')  "##" >> $LF
 echo "##################################" >> $LF
 ##
 ##
-# LF=./../LEEF/0.raw.data/log.$TS.bemovi.mag.16.txt
-# touch $LF 
-# make run.pipeline.bemovi ID=mag.16 &> $LF
-# echo  >> $LF
-# echo "##################################" >> $LF
-# echo "## BEGIN: " $TS "##" >> $LF
-# echo "## END:   " $(date '+%Y-%m-%d--%H-%M-%S')  "##" >> $LF
-# echo "##################################" >> $LF
+LF=./../LEEF/0.raw.data/log.$TS.bemovi.mag.16.txt
+touch $LF 
+make run.pipeline.bemovi ID=mag.16 &> $LF
+echo  >> $LF
+echo "##################################" >> $LF
+echo "## BEGIN: " $TS "##" >> $LF
+echo "## END:   " $(date '+%Y-%m-%d--%H-%M-%S')  "##" >> $LF
+echo "##################################" >> $LF
 ##
