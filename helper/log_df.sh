09d36a4e349a108a7e765a505aeae9ee90dde5ae
Rainer M. Krug <Rainer@krugs.de>
2022-03-01 13:18:55 +0100

    Update log_df.sh

--- helper/log_df.sh
+++ helper/log_df.sh
@@ -9,3 +9,9 @@ do
 	df -hT -x tmpfs -x devtmpfs -x vfat | awk ' {print $1 "," $2 "," $3 "," $4 "," $5 "," $6} ' >> df.log; 
 	sleep $SECONDS; 
 done
+
+####
+# In R
+####
+# read.csv("/Volumes/leef_data/LEEF/df.log") %>% select(Filesystem, Avail, Use.) %>% filter (Filesystem != "Filesystem") %>% filter(Filesystem == "/dev/vda1")
+####
